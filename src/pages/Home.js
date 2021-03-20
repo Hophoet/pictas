fc91e08a0238c1a68feba1b11c525ef9c6c7e193
hophoet <hophoet@gmail.com>
2021-03-20 13:59:30 +0000

    add home page component

---
+++ src/pages/Home.js
@@ -0,0 +1,54 @@
+import { extname } from 'path';
+import React, {useState} from 'react';
+import { useStateValue } from '../redux/StateProvider'
+import { getPictures } from '../api/functions'
+import { useEffect } from 'react';
+import {motion} from 'framer-motion';
+import '../styles/Home.css';
+
+function Home() {
+  const [pictures, setPictures] = useState([]);
+  const [{user}, dispatch] = useStateValue();
+	useEffect(() => {
+    if(user){
+      _getPictures(user.uid)
+      
+    }
+
+	}, [user])	
+
+  const _getPictures = () => {
+    let userId = user.uid;
+    if(userId){
+      getPictures(userId)
+      .then(response => {
+        setPictures(response)
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+  }
+  
+  return (
+    <div className="App">
+      <div className="img-grid">
+        {pictures && pictures.map(picture => (
+          <motion.div className="img-wrap" key={picture.id} 
+            layout
+            whileHover={{ opacity: 1 }}
+            onClick={() => {}}
+          >
+            <motion.img src={picture.url} alt="uploaded pic"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+} 
+export default Home;
