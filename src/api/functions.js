feaa02fc1241cc7faca4b8be993ed948acaf4f4f
hophoet <hophoet@gmail.com>
2021-03-20 22:38:10 +0000

    (api) - add api functions, deletePicture to delete the picture objects, addClient to add clients, updateClient to update client, deleteClient to delete client

--- src/api/functions.js
+++ src/api/functions.js
@@ -1,5 +1,5 @@
 import { resolve } from 'path';
-import { firestore } from '../firebase/config'
+import { firestore, storage } from '../firebase/config'
 
 
 function getPictures(userId){
@@ -28,7 +28,7 @@ function uploadImage(userId, file){
     return new Promise( (resolve, reject) => {
     const picRef = firestore.ref(file.name);
     const colRef = firestore.collection('pictures');
-    const task = picRef.put(file)
+    const task = picRef.putFile(file)
     task.then(() => {
         picRef.getDownloadURL()
         .then((downloadURL) => {
@@ -63,11 +63,35 @@ function setPicture(userId, clientId, url){
         .catch(error => {
             reject(error);
         })
+    })
+}
 
+function deletePicture(id, url){
+    return new Promise( (resolve, reject) => { 
+        storage.refFromURL(url)
+        .delete()
+        .then(response => {
+            firestore
+            .collection('pictures')
+            .doc(id)
+            .delete()
+            .then( response =>{
+                resolve(response);
+            })
+            .catch(error => {
+                reject(error);
+            })
+        })
+        .catch(error => {
+            reject(error);
+        })
     })
 }
 
 
+
+
+//CLIENTS
 function getClients(userId){
    return new Promise( (resolve, reject) => {
        firestore
@@ -89,8 +113,65 @@ function getClients(userId){
    })
 }
 
+function addClient(userId, name, password){
+    return new Promise( (resolve, reject) => {
+        let client = {
+            userId:userId,
+            name:name,
+            password:password
+        }
+        firestore()
+        .collection('clients')
+        .add(client)
+        .then(snapshot => {
+            client.id = snapshot.id;
+            snapshot.set(client);
+        })
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {
+            reject(error)
+        })
 
+    })
+}
 
+function updateClient(clientId, name, password){
+    return new Promise( (resolve, reject) => {
+        let client = {
+            name:name,
+            password:password
+        }
+        firestore()
+        .doc('clients/'+clientId)
+        .update(client => {
+
+        })
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {
+            reject(error);
+        })
+
+    })
+}
+
+function deleteClient(id){
+    return new Promise( (resolve, reject) => { 
+        firestore()
+        .collection('clients')
+        .doc(id)
+        .delete()
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {
+            reject(error);
+        })
+    })
+}
 
 export {
     getPictures,
