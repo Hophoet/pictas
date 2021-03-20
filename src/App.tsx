036b2912af1b96c3993d1cd6c42cb8546eafceec
hophoet <hophoet@gmail.com>
2021-03-20 14:11:28 +0000

    handle the user auth state changing, set the app main route components, authentication and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
+import Auth from './pages/Auth';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { useStateValue } from './redux/StateProvider'
+import { useEffect } from 'react';
+import {auth} from './firebase/config'
+import Header from './pages/Header'
 
 function App() {
+	const [{user}, dispatch] = useStateValue()
+
+	useEffect(() => {
+		auth.onAuthStateChanged(authUser =>{
+			
+			if(authUser){
+				//the user just logged in / the user was logged in 
+				dispatch({
+					type: 'SET_USER',
+					user: authUser
+				})
+			}
+			else{
+				// the user is logged out
+				dispatch({
+					type: 'SET_USER',
+					user: null
+				})
+			}
+		})
+	}, [])	
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/auth">
+            <Auth/>
+          </Route>
+          <Route path="/">
+			<Header/>
+            <Home/>
+          </Route>
+        </Switch>
     </div>
+    </Router>
   );
 }
 
