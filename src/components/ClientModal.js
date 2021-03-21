f6088d305a99f41260ac687bcf8927a1895d666b
hophoet <hophoet@gmail.com>
2021-03-21 21:43:25 +0000

    add update, manage the dynamic saving(new client adding or updating and existence client

--- src/components/ClientModal.js
+++ src/components/ClientModal.js
@@ -2,15 +2,20 @@
 import React, {useState, useEffect} from 'react';
 import { motion } from 'framer-motion';
 import '../styles/ClientModal.css';
-import { addClient } from '../api/functions';
+import { addClient, updateClient } from '../api/functions';
 
-const ClientModal = ({toggleModal, setSelectedPicture, selectedPicture, user }) => {
-    const [client, setClient] = useState([]);
+const ClientModal = ({toggleModal, setSelectedPicture, selectedPicture, user, client}) => {
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
 
     useEffect(() => {
-
+        if(client.name && client.password){
+            setName(client.name);
+            setPassword(client.password);
+        }
+        console.log(client)
+        console.log(name)
+        console.log(password)
     }, [])	
     
     const handleClick = (e) => {
@@ -36,23 +41,45 @@ const ClientModal = ({toggleModal, setSelectedPicture, selectedPicture, user }) 
 
     const save = () => {
        if(_checkInput()){
-            add();
+            if(client.name && client.password){
+                console.log('update')
+                update();
+            }
+            else{
+                add();
+            }
        } 
 
     }
 
     const add = () => {
         //alert(user.uid)
-       addClient(user.uid, name, password) 
+       addClient(user.uid, name, password)
        .then(response => {
-           console.log('client saved')
-           console.log(response)
+           console.log('client saved');
+           console.log(response);
+           toggleModal();
        })
        .catch(error => {
            console.log('client saved failed')
            console.log(error);
        })
     }
+
+    const update = () => {
+        updateClient(client.id)
+        .then(response => {
+            console.log('client updated');
+            console.log(response);
+        })
+       .catch(error => {
+           console.log('client updated failed')
+           console.log(error);
+       })
+
+    }
+
+
 
   return (
     <motion.div onClick={handleClick} className="backdrop" 
@@ -67,15 +94,15 @@ const ClientModal = ({toggleModal, setSelectedPicture, selectedPicture, user }) 
             
                     <div className="imagecontainer">
                             <div className="output">
-                                <input placeholder='Client name'
+                                <input value={name} placeholder='Client name'
                                     onChange={
                                         e => setName(e.target.value)}
                                 />
-                                <input type='text' placeholder='Client password'
+                                <input value={password} type='text' placeholder='Client password'
                                     onChange={
                                         e => setPassword(e.target.value)}
                                 />
-                                <button onClick={save}>ajouter</button>
+                                <button onClick={save}>{(client.name && client.password)?'update':'add'}</button>
                             </div>
                     </div>
                 </motion.div>
